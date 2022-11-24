b0c635b4f9aad376a50853d28bd6d8422957b46f
yehor.trembovetskyi <yehor.trembovetskyi@leovegas.com>
2022-11-24 20:04:02 +0200

    Fix checking not emptiness; Add error handling

--- main.py
+++ main.py
@@ -19,50 +19,54 @@ if __name__ == '__main__':
     start_time = time.time()
 
     for leo_admin_game in leo_admin_games:
-        i += 1
-        print('\n-----------------------------------------\n')
-        print(f'Start processing game #{i}/{len_leo_admin_games} (uniqueId={leo_admin_game["uniqueId"]}, name={leo_admin_game["name"]})')
-        lemur_game = client.get_game(leo_admin_game['uniqueId'])
+        try:
+            i += 1
+            print('\n-----------------------------------------\n')
+            print(f'Start processing game #{i}/{len_leo_admin_games} (uniqueId={leo_admin_game["uniqueId"]}, name={leo_admin_game["name"]})')
+            lemur_game = client.get_game(leo_admin_game['uniqueId'])
 
-        if 'errors' in lemur_game:
-            update_failed += 1
+            if 'errors' in lemur_game:
+                update_failed += 1
 
-            for e in lemur_game['errors']:
-                if "No game was found with id: " in e['message']:
-                    p_failed({
-                        'message': 'Lemur doesnt have game with id',
-                        'uniqueId': leo_admin_game['uniqueId']
-                    })
-                    print('-- NON-EXISTING GAME CASE --')
-                    continue
+                for e in lemur_game['errors']:
+                    if "No game was found with id: " in e['message']:
+                        p_failed({
+                            'message': 'Lemur doesnt have game with id',
+                            'uniqueId': leo_admin_game['uniqueId']
+                        })
+                        print('-- NON-EXISTING GAME CASE --')
+                        continue
 
-            p_failed({
-                'unique_id': leo_admin_game['uniqueId'],
-                'lemur_response': lemur_game,
-                'leo_admin_game': leo_admin_game
-            })
-            print('-- REQUEST FAILED --')
+                p_failed({
+                    'unique_id': leo_admin_game['uniqueId'],
+                    'lemur_response': lemur_game,
+                    'leo_admin_game': leo_admin_game
+                })
+                print('-- REQUEST FAILED --')
+                continue
+
+            lemur_game = lemur_game['data']['gameInformation']
+
+            print(f'Lemur game config: {to_json(lemur_game)}')
+
+            lemur_game = update_game_dga_input(lemur_game, leo_admin_game)
+            print(f'Updated lemur game config: {to_json(lemur_game)}')
+            response = client.update_game(lemur_game)
+            print(f'Lemur response: {to_json(response)}')
+
+            if is_success(response):
+                successful += 1
+            else:
+                update_failed += 1
+                p_failed({
+                    'unique_id': leo_admin_game['uniqueId'],
+                    'lemur_response': response,
+                    'leo_admin_game': leo_admin_game
+                })
+                print('-- REQUEST FAILED --')
+        except Exception as e:
+            print(e)
             continue
-
-        lemur_game = lemur_game['data']['gameInformation']
-
-        print(f'Lemur game config: {to_json(lemur_game)}')
-
-        lemur_game = update_game_dga_input(lemur_game, leo_admin_game)
-        print(f'Updated lemur game config: {to_json(lemur_game)}')
-        response = client.update_game(lemur_game)
-        print(f'Lemur response: {to_json(response)}')
-
-        if is_success(response):
-            successful += 1
-        else:
-            update_failed += 1
-            p_failed({
-                'unique_id': leo_admin_game['uniqueId'],
-                'lemur_response': response,
-                'leo_admin_game': leo_admin_game
-            })
-            print('-- REQUEST FAILED --')
 
     print('\n-----------------------------------------\n')
     print(f"Execution time: {format_time(time.time() - start_time)}")
